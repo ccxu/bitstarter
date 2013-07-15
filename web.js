0f114b7dab6cac9f71a6ce8e534339f529ae3f8e
gayka <gayka@mail.com>
2013-07-15 20:15:54 +0000

    read index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs =require('fs');
+var inputFile="index.html";
+var data = fs.readFileSync(ÑinputFile);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  
+response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
